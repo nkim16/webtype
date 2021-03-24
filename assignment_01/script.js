07c47bfd1d0926291df6296378530f26240f1812
nkim16 <nkim16@risd.edu>
2021-03-24 19:42:10 -0400

    update_3/24

---
+++ assignment_01/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    $("#sf_button").hide();
+    $("#la_button").hide();
+    $("#chicago_button").hide();
+    $("#boston_button").hide();
+    $("#providence_button").hide();
+    $("#nyc_button").hide();
+    $("#menu_button").hide();
+    $("#about_button").hide();
+
+
+    $("#circle_button").click(function () {
+        $("#sf_button").show();
+        $("#la_button").show();
+        $("#chicago_button").show();
+        $("#boston_button").show();
+        $("#providence_button").show();
+        $("#nyc_button").show();
+        $("#menu_button").show();
+        $("#about_button").show();
+
+
+    });
+
+
+}); 
